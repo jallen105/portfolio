706179aba72a86782dd96b265c79e67838badd7b
Jonathan <jonathanallen105@gmail.com>
2025-10-28 16:46:57 -0400

    added the projects data file and style the project card.

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
 
 const Project = () => {
   return (
-    <div>Project details here.</div>
+    <div className="card">
+      <div className="card-front">
+        <h3>project title</h3>
+        <img src="" alt="pic of app" />
+      </div>
+      <div className="card-back">
+        <p>description</p>
+        <p>tech used</p>
+        <p>dev time</p>
+        <p>link to deployed project</p>
+        <p>repo link</p>
+      </div>     
+
+    </div>
   )
 }
 
