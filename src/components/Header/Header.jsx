83adb773ad17542ce63442d7feab937cc6910408
Jonathan <jonathanallen105@gmail.com>
2025-10-30 16:21:00 -0400

    styled the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,11 @@ const Header = ({ img }) => {
   return (
     <header>
         <img src={img} alt="a picture of me." />
-        <h1>Jonathan Allen</h1>
-        <h3>From IT help desk to Full Stack Software Engineer — growing, building, and having fun along the way.</h3>
+        <div className="header-content">
+          <h1>Jonathan Allen</h1>
+          <h3>From IT help desk to Full Stack Software Engineer — growing, building, and having fun along the way.</h3>
+        </div>
+
     </header>
   )
 }
