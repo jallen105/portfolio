eabecba9ca6777b9c02736a80e0900ccb6f33704
Jonathan <jonathanallen105@gmail.com>
2025-10-30 18:33:23 -0400

    styled the card for projects

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -3,20 +3,17 @@ const Project = ({ project, img }) => {
 
   return (
     <div className="card">
-      <div className="card-front">
-        <h3>{ project.title }</h3>
-        <img src={img} alt="pic of app" />
-      </div>
-      <div className="card-back">
-        <p>{ project.description }</p>
-        <ul>
-        { project.technologies.map((technology, idx) => (
-          <li key={idx}>{technology}</li>
-        ))}
-        </ul>
-        <p><a href={ project.siteLink } >Deployed App</a></p>
-        <p><a href={ project.repoLink } >Git Hub repo</a></p>
-      </div>     
+      <h3 className="title">{ project.title }</h3>
+      <img src={img} alt="pic of app" />
+      <p>{ project.description }</p>
+      <p><b>Technologies</b></p>
+      <ul>
+      { project.technologies.map((technology, idx) => (
+        <li key={idx}>{technology}</li>
+      ))}
+      </ul>
+      <p><a href={ project.siteLink } >Deployed App</a></p>
+      <p><a href={ project.repoLink } >Git Hub Repo</a></p>
     </div>
   )
 }
